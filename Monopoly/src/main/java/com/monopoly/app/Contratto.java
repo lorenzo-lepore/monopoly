cb4bf62b11bd1f6a4a9d72fe12494d13e246e367
lorenzo.lepore <91348041+lorenzo-lepore@users.noreply.github.com>
2023-05-11 22:13:04 +0200

    Bug fixing & improved readability

---
+++ Monopoly/src/main/java/com/monopoly/app/Contratto.java
@@ -0,0 +1,69 @@
+package com.monopoly.app;
+
+import java.io.Serializable;
+
+/**
+ * Classe rappresentante un contratto utilizzabile dal giocatore. Il contratto può essere acquistato ed usato per riscuotere
+ * dai non possessori tariffe prestabilite quando essi passano sopra la corrispondente casella del tabellone.
+ * Implementa l'interfaccia {@link Serializable} per salvare lo stato di ogni oggetto di tipo {@code Contratto} su file.
+ *
+ * @author Lorenzo Lepore - Matricola: 172416
+ * @version 1.0
+ */
+@SuppressWarnings("SpellCheckingInspection")
+public class Contratto implements Serializable {
+    private final String nome;
+    private final int prezzoVendita;
+    private final int costoAffitto;
+    private boolean disponibile;
+    private Giocatore proprietario;
+
+    /**
+     * Istanzia un oggetto di tipo {@code Contratto}. Inizialmente il contratto è sempre disponibile, e non possiede proprietario.
+     *
+     * @param nome nome del contratto
+     * @param prezzoVendita prezzo di vendita del contratto
+     * @param costoAffitto costo di affitto del contratto
+     * @param disponibile indica se un contratto è acquistabile o meno
+     *
+     */
+    public Contratto(String nome, int prezzoVendita, int costoAffitto, boolean disponibile){
+        this.nome = nome;
+        this.prezzoVendita = prezzoVendita;
+        this.costoAffitto = costoAffitto;
+        this.disponibile = disponibile;
+        this.proprietario = null;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getPrezzoVendita() {
+        return prezzoVendita;
+    }
+
+    public int getCostoAffitto() {
+        return costoAffitto;
+    }
+
+    public boolean getDisponibile() {
+        return disponibile;
+    }
+
+    public Giocatore getProprietario() {
+        return proprietario;
+    }
+
+    public void setProprietario(Giocatore proprietario) {
+        this.proprietario = proprietario;
+    }
+
+    public void setDisponibile(boolean disponibile) {
+        this.disponibile = disponibile;
+    }
+
+    public String toString() {
+        return "- " + this.nome;
+    }
+}
